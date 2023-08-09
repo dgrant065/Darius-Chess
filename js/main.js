d550fe4803e1e73449f44d055f54be081598b529
Darius Grant <d.grant065@gmail.com>
2023-08-08 20:46:31 -0400

    Add 'Piece' class in js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
   console.log("js is working")
-  
+
   /*----- constants -----*/
 const PLAYERS = {
   '0': '',
@@ -7,10 +7,26 @@ const PLAYERS = {
   '-1': 'Black',
 }
 
+// const bKing = '<div class="piece" id="king">"https://cdn.discordapp.com/attachments/485962663080230912/1138247122525892638/Chess_qdt60.png"</div>' 
+// const bQueen = '<div class="piece" id="queen">"https://cdn.discordapp.com/attachments/485962663080230912/1138247369964650617/Chess_kdt60.png"</div>'
+// const bBishop = '<div class="piece" id="bishop">"https://cdn.discordapp.com/attachments/485962663080230912/1138247431084052581/Chess_bdt60.png"</div>'
+// const bKnight = '<div class="piece" id="knight">"https://cdn.discordapp.com/attachments/485962663080230912/1138247473492676719/Chess_ndt60.png"</div>'
+// const bRook = '<div class="piece" id="rook">"https://cdn.discordapp.com/attachments/485962663080230912/1138247516232630353/Chess_rdt60.png"</div>'
+// const bPawn = '<div class="piece" id="pawn">"https://cdn.discordapp.com/attachments/485962663080230912/1138247558121144465/Chess_pdt60.png"</div>'
+
+// const wKing = '<div class="piece" id="king">"https://cdn.discordapp.com/attachments/485962663080230912/1138247601351823460/Chess_qlt60.png"</div>'
+// const wQueen = '<div class="piece" id="queen">"https://cdn.discordapp.com/attachments/485962663080230912/1138247651108868146/Chess_klt60.png"</div>'
+// const wBishop = '<div class="piece" id="bishop">"https://cdn.discordapp.com/attachments/485962663080230912/1138247667827359754/Chess_blt60.png"</div>'
+// const wKnight = '<div class="piece" id="knight">"https://cdn.discordapp.com/attachments/485962663080230912/1138247703042723900/Chess_nlt60.png"</div>'
+// const wRook = '<div class="piece" id="rook">"https://cdn.discordapp.com/attachments/485962663080230912/1138247738887241738/Chess_rlt60.png"</div>'
+// const wPawn = '<div class="piece" id="pawn">"https://cdn.discordapp.com/attachments/485962663080230912/1138247765743390830/Chess_plt60.png"</div>'
 
   /*----- state variables -----*/
-let board, turn, winner
+let board, turn, winner, blackPieces, whitePieces
 
+// classes
+
+//create a 'Piece' class, every piece should have a location property that stores an object, that looks like this {x: 0, y: 0}, 
   /*----- cached elements  -----*/
 const messageEl = document.querySelector('h1')
 const playAgainBtn = document.querySelector('button')
@@ -20,24 +36,25 @@ const boardEls = [...document.querySelectorAll('#board > div')]
 playAgainBtn.addEventListener('click', init);
 
   /*----- functions -----*/
-  init();
+init();
 
-  function init() {
+function init() {
 
-    // board = [
-    //     // [0, 0, 0, 0, 0, 0 ,0, 0],
-    //     // [0, 0, 0, 0, 0, 0 ,0, 0],
-    //     // [0, 0, 0, 0, 0, 0 ,0, 0],
-    //     // [0, 0, 0, 0, 0, 0 ,0, 0],
-    //     // [0, 0, 0, 0, 0, 0 ,0, 0],
-    //     // [0, 0, 0, 0, 0, 0 ,0, 0],
-    //     // [0, 0, 0, 0, 0, 0 ,0, 0],
-    //     // [0, 0, 0, 0, 0, 0 ,0, 0]
-    // ];
-    turn = 1;
-    winner = null;
-    render();
-  }
+  board = [
+    [0, 0, 0, 0, 0, 0 ,0, 0],
+    [0, 0, 0, 0, 0, 0 ,0, 0],
+    [0, 0, 0, 0, 0, 0 ,0, 0],
+    [0, 0, 0, 0, 0, 0 ,0, 0],
+    [0, 0, 0, 0, 0, 0 ,0, 0],
+    [0, 0, 0, 0, 0, 0 ,0, 0],
+    [0, 0, 0, 0, 0, 0 ,0, 0],
+    [0, 0, 0, 0, 0, 0 ,0, 0]
+  ];
+  buildPieces()
+  turn = 1;
+  winner = null;
+  render();
+}
 
 
 function render() {
@@ -79,3 +96,53 @@ function renderMessage() {
 function renderControls() {
   playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
 }
+
+function buildPieces() {
+
+}
+
+class Piece {
+  constructor(column, row, color) {
+    this.column = column;
+    this.row = row;
+    this.color = color;
+  }
+}
+
+class Pawn extends Piece () {
+  move()
+    
+}
+
+class King extends Piece() {
+  move()
+}
+
+class Queen extends Piece() {
+  move()
+}
+
+class Rook extends Piece() {
+  move()
+}
+
+class Bishop extends Piece() {
+  move()
+}
+
+class Knight extends Piece() {
+  move()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
